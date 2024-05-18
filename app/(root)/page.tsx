d21e7409c18b93e59830bb5453daa672011a1e17
YannickAmoikon <amoikon44@gmail.com>
2024-05-18 15:23:35 +0000

    first

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Header from "@/app/components/Header";
+
+export default function Home() {
+    return (
+        <>
+            <div>
+                {/*Header*/}
+                <Header/>
+            </div>
+        </>
+    );
+}
